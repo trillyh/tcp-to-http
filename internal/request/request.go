2978242a08c5ba0078c29cf128dcbbb6c2791abb
trillyh <trillionhoang0308@gmail.com>
2025-09-22 19:17:35 -0700

    Update the read request to read in chunk instead of readAll

--- internal/request/request.go
+++ internal/request/request.go
@@ -30,8 +30,21 @@ type RequestLine struct {
 	Method        string
 }
 
+type parserState string
+const (
+	initialzed = "init"
+	done = "done"
+)
+
 type Request struct {
 	RequestLine RequestLine // Ex: GET /coffee HTTP/1.1
+	state parserState
+}
+
+func newRequest() *Request {
+	return &Request {
+		state: initialzed,
+	}
 }
 
 var ErrBadRequestLine = fmt.Errorf("bad request-line")
@@ -40,9 +53,11 @@ var ErrUnsupportedVersion = fmt.Errorf("unsupported HTTP version")
 var ErrInvalidMethod = fmt.Errorf("invalid method")
 var SEPARATOR = "\r\n"
 
+func (r *Request) parse(data []byte) (int, error) {
 
+}
 
-func parseRequestLine(s string) (*RequestLine, string, error) {
+func parseRequestLine(s string) (*RequestLine, int, error) {
 	idx := strings.Index(s, SEPARATOR)
 	if idx == -1 {
 		return nil, s, nil
@@ -79,20 +94,33 @@ func parseRequestLine(s string) (*RequestLine, string, error) {
 }
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	requestBytes, err := io.ReadAll(reader)
+	request := newRequest()
 
-	if err != nil {
-		return nil, fmt.Errorf("failed to io.ReadAll")
+	// buf could overrun when header + body > 1k 
+	buf := make([]byte, 1024)
+	bufLen := 0 // valid bytes currently in the buffer
+	for request.state != "done" {
+		// Read from bufLen(start from 0) to 1024
+		// n is the number of bytes it has read (element in the buf right now)
+		n, err := reader.Read(buf[bufLen:]) 
+		if err != nil {
+			return nil, err
+		}
+
+		bufLen += n 
+		// pass in the buf from 0->bufLen to the request's parser
+		// parseN is the number of bytes the request parsed (read)
+		parsedN, err := request.parse(buf[:bufLen])
+		if err != nil {
+			return nil, err
+		}
+
+		// data from bufLen up to parsedN is already consumed, so no longer need 0<----bufLen and update bufLen
+		// buf[parsedN:bufLen] are still unparsed leftovers
+		// shift left (use big brain)
+		copy(buf, buf[parsedN:bufLen])
+		bufLen -= parsedN 	
 	}
 
-	requestStr := string(requestBytes)
-
-	requestLine, _, err := parseRequestLine(requestStr)
-	if err != nil {
-		return nil, err
-	}
-
-	return &Request{
-		RequestLine: *requestLine, // <--------- panic if requestLine is nil 
-	}, err
+	return request, nil
 }
