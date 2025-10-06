1b69ac7cac078992cd16723d8d2c0f9f2bf80220
trillyh <trillionhoang0308@gmail.com>
2025-10-05 20:08:15 -0700

    First draft https

--- internal/server/main.go
+++ internal/server/main.go
@@ -1,26 +1,56 @@
 package server
 
 import (
+	"fmt"
+	"log"
 	"net"
+	"strconv"
 )
 
 type Server struct {
-
+	listener net.Listener
 }
 
-//Creates a net.Listener and returns a new Server instance. Starts listening for requests inside a goroutine.
-func Serve(port int) (*Server, error) 
+// Creates a net.Listener and returns a new Server instance. Starts listening for requests inside a goroutine.
+func Serve(port int) (*Server, error)  {
+	portStr := ":" + strconv.Itoa(port)
+	listener, err := net.Listen("tcp", portStr)
+	if err != nil {
+		return nil, fmt.Errorf("error when creating listener for port %d", port)
+	}
+	server := &Server {
+		listener: listener,
+	}
+	go server.listen()
+	return server, nil
+}
 
-
-func (s *Server) Close() error 
+func (s *Server) Close() error {
+	err := s.listener.Close()
+	return err
+}
 
 /* 
 Uses a loop to .Accept new connections as they come in, and handles each one in a new goroutine. 
 I used an atomic.Bool to track whether the server is closed or not so that I can ignore connection errors after the server is closed.
 */
-func (s *Server) listen()
+func (s *Server) listen() {
+	 for {
+		conn, err := s.listener.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("New connection accepted")
+		go s.handle(conn)
+	}
+}
 
 /*
 Handle single conection then close
 */
-func (s *Server) handle(conn net.Conn)
+func (s *Server) handle(conn net.Conn) {
+	fmt.Println("Handling the new connection")
+	response := []byte("HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 13\nHello World!")
+	conn.Write(response)
+	conn.Close()
+}
