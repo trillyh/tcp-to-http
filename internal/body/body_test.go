f51e6b82376400a8cf71376332abe4c2f3c45681
trillyh <trillionhoang0308@gmail.com>
2025-10-16 10:30:00 -0700

    Comment Body

--- internal/body/body_test.go
+++ internal/body/body_test.go
@@ -7,6 +7,7 @@ import (
 )
 
 func TestBody(t *testing.T) {
+	// Test standard ok
 	body := NewBody()
 	data := []byte("hello world @!@\n")
 	body.ContentLength = len(data)
@@ -16,4 +17,15 @@ func TestBody(t *testing.T) {
 	assert.Equal(t, "hello world @!@\n", body.Body)
 	assert.Equal(t, 16, n)
 	assert.True(t, done)
+
+
+	// Test contentLength is larger than body
+	body = NewBody()
+	data = []byte("hello world @!@\n")
+	body.ContentLength = 18
+	_, done, err = body.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, body)
+	assert.Equal(t, "hello world @!@\n", body.Body)
+	assert.False(t, done)
 }
